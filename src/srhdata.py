f696ec3f758e3b1411cc0cc9d475292f1f5025e9
Maria Globa <mvgloba@gmail.com>
2022-08-06 15:47:14 +0800

    add srhFits module

--- src/srhdata.py
+++ src/srhdata.py
@@ -5,7 +5,7 @@ Created on Tue Aug  2 16:50:21 2022
 
 @author: maria
 """
-from srhFits import SrhFitsFile
+from srhFits.srhFits import SrhFitsFile
 
 def open(filename):
     srhFitsObj = SrhFitsFile(filename)
