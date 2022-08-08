c307bf16cc272f76f19ab91ab3863925b67cede6
Мария Глоба <globa@iszf.irk.ru>
2022-08-08 17:18:41 +0800

    rename

--- srhData/srhData.py
+++ srhData/srhData.py
@@ -5,11 +5,8 @@ Created on Tue Aug  2 16:50:21 2022
 
 @author: maria
 """
-<<<<<<< HEAD
-from srhFits import SrhFitsFile
-=======
+
 from srhFits.srhFits import SrhFitsFile
->>>>>>> 5bc4c92d5a6af9ca52690b9017a0b0e4a794ec67
 
 def open(filename):
     srhFitsObj = SrhFitsFile(filename)
