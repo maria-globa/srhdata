8ae49f7f1b4231709f3afd9aa3a7096af8cdc0a5
maria-globa <mvgloba@gmail.com>
2024-08-19 14:37:43 +0800

    modified execute_task

--- src/srhdata/srh_data.py
+++ src/srhdata/srh_data.py
@@ -66,11 +66,11 @@ def execute_task(synth_task):
     srh_obj.makeImage(path = synth_task['params']['outdir'],
                       frequency = synth_task['task'][0]['frequency_index'],
                       average = srh_obj.dataLength,
-                      compress_image = synth_task['params']['compressed'],
+                      #compress_image = synth_task['params']['compressed'],
                       RL = out_pol,
                       clean_disk = synth_task['params']['clean_disk'],
-                      cell = synth_task['params']['cdelt'],
-                      imsize = synth_task['params']['naxis'])
+                      cdelt = synth_task['params']['cdelt'],
+                      naxis = synth_task['params']['naxis'])
     if out_pol:
         return {'R' : srh_obj.out_filenames[0], 'L' : srh_obj.out_filenames[1]}
     else:
